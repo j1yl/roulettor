66709d7f402093cd1472d21094475134ed336d55
Joe Lee <j1yl@pm.me>
2023-04-14 00:30:23 -0700

    Update server code

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,31 @@ import axios from "axios";
 
 /**
  *
+ * TYPES
+ *
+ */
+interface GameInfo {
+  id: string;
+  timeRemaining: number;
+  gameState?: "STARTED" | "ENDED" | "PREPARING";
+  winningColor?: "red" | "black" | "green" | "none";
+  winningValue?: number;
+  bets: BetInfo[];
+}
+
+interface BetInfo {
+  id: string;
+  status: "won" | "lost" | "pending";
+  userId: string;
+  gameId: string;
+  betColor: string;
+  betAmount: number;
+  payout: number;
+  createdAt?: Date;
+}
+
+/**
+ *
  * INITIALIZATION
  *
  */
@@ -17,7 +42,20 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-  console.log("User connected", socket.id);
+  console.log(">>>>>USER CONNECTED", socket.id);
+  socket.on("betPlaced", (data: BetInfo) => {
+    gameData.bets.push(data);
+    // {
+    //   id: 'clgft1bsj0003liz5und7hovi',
+    //   status: 'pending',
+    //   userId: 'clgfp0jlb000ulifi47ckhaxn',
+    //   gameId: 'clgft15cg0000liz5azljwc28',
+    //   betColor: 'green',
+    //   betAmount: 100,
+    //   createdAt: '2023-04-14T00:22:06.835Z'
+    // }
+    socket.emit("betReceived", gameData.bets);
+  });
 });
 
 httpServer.listen(3001, () => {
@@ -30,28 +68,17 @@ httpServer.listen(3001, () => {
 
 /**
  *
- * TYPES
+ * DEFAULT GAME DATA
  *
  */
-interface GameInfo {
-  timeRemaining: number;
-  id: string;
-  gameState?: "STARTED" | "ENDED";
-  winningColor?: "red" | "black" | "green" | "none";
-  winningValue?: number;
-  bets: [];
-}
-
-interface BetInfo {
-  id: string;
-  userId: string;
-  gameId: string;
-  betColor: string;
-  betAmount: number;
-  createdAt: Date;
-}
-
-let gameData = {} as GameInfo;
+let gameData: GameInfo = {
+  id: "",
+  timeRemaining: 0,
+  gameState: "PREPARING",
+  winningColor: "none",
+  winningValue: 0,
+  bets: [],
+};
 
 /**
  *
@@ -76,50 +103,98 @@ const getWinningColor = (value: number) => {
 
 const sendGameUpdate = (data: GameInfo) => {
   io.emit("gameUpdate", data);
-  console.log(new Date().toUTCString(), "Emitted gameUpdate", data);
+  console.log(
+    `TIME: ${data.timeRemaining.toString()}\nGAME UPDATE: ${
+      data.gameState
+    }\nGAME ID: ${data.id}\n`
+  );
 };
 
-const sendTimeUpdate = (currentSeconds: number) => {
-  io.emit("timeUpdate", currentSeconds);
-  // console.log(new Date().toUTCString(), "Emitted timeUpdate", currentSeconds);
-};
-
-timer.addEventListener("secondsUpdated", async (e) => {
-  let currentSeconds = timer.getTimeValues().seconds;
-  gameData.timeRemaining = currentSeconds;
-  // console.log(timer.getTimeValues().toString());
-  sendTimeUpdate(Math.abs(currentSeconds - 60));
+timer.addEventListener("secondsUpdated", (e) => {
+  gameData = {
+    ...gameData,
+    timeRemaining: timer.getTimeValues().seconds,
+  };
 
   if (timer.getTimeValues().toString() === "00:01:00") {
-    // CREATE NEW GAME
-    gameData.timeRemaining = 59;
+    /**
+     * CREATE NEW GAME
+     */
     gameData.gameState = "STARTED";
-    sendGameUpdate(gameData);
+    try {
+      axios
+        .post("http://localhost:3000/api/roulette/create", gameData)
+        .then((res) => {
+          gameData = {
+            ...gameData,
+            id: res.data.data.id,
+          };
+        });
+    } catch (e) {
+      console.error(e);
+    }
   } else if (timer.getTimeValues().toString() === "00:00:00") {
-    // END GAME
+    /**
+     * END GAME
+     */
     gameData.gameState = "ENDED";
-    gameData.winningValue = getRandomInt(14);
+    gameData.winningValue = getRandomInt(14); // change to more secure random number gen
     gameData.winningColor = getWinningColor(gameData.winningValue);
+    /**
+     * PAYOUT
+     */
+    try {
+      axios.get("http://localhost:3000/api/roulette/payout").then((res) => {});
+    } catch (e) {
+      console.error(e);
+    }
+
+    gameData.bets.forEach((item) => {
+      if (item.betColor === gameData.winningColor) {
+        item.status = "won";
+      } else {
+        item.status = "lost";
+      }
+      try {
+        axios
+          .post("http://localhost:3000/api/roulette/save/bet", item)
+          .then((res) => {
+            // payout !!!
+            gameData.bets = [];
+          });
+      } catch (e) {
+        console.error(e);
+      }
+    });
 
     try {
-      const res = await axios.post(
-        "http://localhost:3000/api/roulette/save",
-        gameData
-      );
-
-      res.data.data.timeRemaining = 60 - gameData.timeRemaining;
-      sendGameUpdate(res.data.data);
+      axios
+        .post("http://localhost:3000/api/roulette/save/game", gameData)
+        .then((res) => {
+          gameData = {
+            ...gameData,
+            id: res.data.data.id,
+          };
+        });
     } catch (e) {
       console.error(e);
     }
   }
+  sendGameUpdate(gameData);
 });
 
+/**
+ *
+ * PAUSE THE GAME FOR 5 SECONDS
+ *
+ */
 timer.addEventListener("targetAchieved", () => {
-  console.log("Pausing for 5 seconds");
+  console.log(
+    `TIMER ${timer.getTimeValues().toString()} PAUSING FOR 5 SECONDS`
+  );
   timer.pause();
   setTimeout(() => {
-    console.log("Resuming", timer.getTimeValues().toString());
+    console.log(`TIMER ${timer.getTimeValues().toString()} RESUMING THE GAME`);
     timer.reset();
     timer.start({
       precision: "seconds",
