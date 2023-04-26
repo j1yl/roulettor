6456c5c7ebaf6488d58c49485a338613a444a12e
Joe Lee <lefodev@gmail.com>
2023-04-25 17:16:36 -0700

    Change * cors to CLIENTURL

--- server/app.ts
+++ server/app.ts
@@ -36,7 +36,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: process.env.CLIENTURL,
   },
 });
 const timer = new Timer();
